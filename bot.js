a1f8a6823a096388dbe3f64db4d5a71f473b9dec
sezerz1121 <85015473+sezerz1121@users.noreply.github.com>
2024-03-21 21:42:37 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -38,8 +38,7 @@ async function startServer(message,onlineStatus) {
       await page.waitForSelector('.servercard.offline');
       await page.click('.servercard.offline');
       await page.waitForTimeout(2000);
-      await page.waitForSelector('.btn.btn-white.CspDLDFOEQRw');
-      await page.click('.btn.btn-white.CspDLDFOEQRw');
+      
       await page.waitForTimeout(5000);
       await page.waitForSelector('#start');
       await page.click('#start');
